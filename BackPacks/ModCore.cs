8f103e3d251ec647c16da4f1fb3aaa26dca6b0a6
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-02-13 19:59:15 -0700

    Stage code to support ItemManager and ServerSync

--- BackPacks/ModCore.cs
+++ BackPacks/ModCore.cs
@@ -1,22 +1,71 @@
 ﻿using System.Reflection;
 using BepInEx;
 using HarmonyLib;
+using ItemManager;
+using UnityEngine;
 
 namespace BackPacks
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class NewMod : BaseUnityPlugin
+    public class BackPacks : BaseUnityPlugin
     {
-        private const string ModName = "New Mod";
-        private const string ModVersion = "1.0";
-        private const string ModGUID = "some.new.guid";
+        internal const string ModName = "BackPacks";
+        internal const string ModVersion = "0.0.1";
+        private const string ModGUID = "com.zarboz.backpacks";
         private static Harmony harmony = null!;
         
+#pragma warning disable CS0649
+        internal static AssetBundle? eviesBackPacks;
+        internal static AssetBundle? backpackDrops;
+#pragma warning restore CS0649
+        
+        internal static Item? IronBag;
+        internal static Item? SilverBag;
+
+
         public void Awake()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
             harmony = new(ModGUID);
             harmony.PatchAll(assembly);
+
+            SetupIronBag();
+            SetupSilverBag();
+        }
+
+        private void SetupIronBag()
+        {
+            IronBag = new Item("backpacks", "CapeIronBackpack", "Assets");
+            //Localization
+            IronBag.Name.English("Rugged Backpack");
+            IronBag.Description.English("A Rugged backpack, complete with buckles and fine leather straps.");
+            //Crafting
+            IronBag.Crafting.Add(CraftingTable.Workbench, 1);
+            IronBag.RequiredItems.Add("Iron", 10);
+            IronBag.RequiredItems.Add("LeatherScraps", 15);
+            IronBag.RequiredItems.Add("LinenThread", 5);
+            IronBag.CraftAmount = 1;
+            //Upgrades
+            IronBag.RequiredUpgradeItems.Add("Iron", 20);
+            IronBag.RequiredUpgradeItems.Add("LinenThread", 5);
+            
+        }
+
+        private void SetupSilverBag()
+        {
+            SilverBag = new Item("backpacks", "CapeSilverBackpack", "Assets");
+            //Localization
+            SilverBag.Name.English("Fine Backpack");
+            SilverBag.Description.English("A Fine backpack, complete with hand made straps");
+            //Crafting
+            SilverBag.Crafting.Add(CraftingTable.Workbench, 1);
+            SilverBag.RequiredItems.Add("Iron", 10);
+            SilverBag.RequiredItems.Add("LeatherScraps", 15);
+            SilverBag.RequiredItems.Add("LinenThread", 5);
+            SilverBag.CraftAmount = 1;
+            //Upgrades
+            SilverBag.RequiredUpgradeItems.Add("Iron", 20);
+            SilverBag.RequiredUpgradeItems.Add("LinenThread", 5);
         }
         
         
