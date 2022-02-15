c2c4564e13249142e400189916ca93fb072c8a6f
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-02-15 12:02:42 -0700

    Screwing with setting a value in editor

---
+++ BackPack_UnityProject/Assets/Scripts/ValueSetter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ValueSetter : MonoBehaviour
+{
+    [SerializeField] internal BackPack Pack;
+    [SerializeField] internal ZNetView m_nview;
+    
+    
+    private void Awake()
+    {
+        Pack.originalDrop = this.gameObject;
+        Pack.ItemDataref = gameObject.GetComponent<ItemDrop>();
+    }
+    
+    public string mGuid = System.Guid.NewGuid().ToString();
+}
